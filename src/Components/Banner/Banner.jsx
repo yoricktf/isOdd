da5b250040b10bbd0e1fe29a3bd2add61a697935
Yorick ten Feld <yorick.tenfeld@gmail.com>
2022-09-14 21:46:12 +0200

    added all the components required for the top banner

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import './Banner.css'
 import Button from 'react-bootstrap/Button'
-import Bannergrphic from '../../BannerGraphic.svg'
+import Bannergraphic from '../../BannerGraphic.svg'
+import wave from '../../wave.svg'
 
 const Banner = () => {
   return (
-    <div id='banner'>
-      <div className="bannerInfo">
+    <>
 
-        <h1>isOdd API</h1>
-        <p>use isOdd to tell if a number is odd</p>
-        <Button>View Docs</Button>
+      <div id='banner'>
+        <div className='bannerInfo'>
+          <h1>isOdd API</h1>
+          <p>use isOdd to tell if a number is odd</p>
+          <Button>View Docs</Button>
+        </div>
+        <img className='bannerGraphic' src={Bannergraphic} alt='banner graphic' />
+
+
       </div>
-      <img src={Bannergrphic} alt="banner graphic" />
-    </div>
+      <img className='wavyEdge' src={wave} alt='wave graphic' />
+    </>
   )
 }
 
