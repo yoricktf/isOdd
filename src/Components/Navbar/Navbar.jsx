5e41a28b90755082b47b6f336a3011e474913bc8
Yorick ten Feld <yorick.tenfeld@gmail.com>
2022-09-14 10:38:58 +0200

    created navbar component files and added the navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Navbar.css'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+
+const NavbarComponent = () => {
+  return (
+    <Navbar className='navbar' expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">isOdd</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Features</Nav.Link>
+            <Nav.Link href="#link">Docs</Nav.Link>
+            <Nav.Link href="#link">Pricing</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+
+    </Navbar>
+  )
+}
+
+export default NavbarComponent
