902076101bd1e78d9174083c3e958f6e93229809
Yorick ten Feld <yorick.tenfeld@gmail.com>
2022-09-15 09:55:54 +0200

    added client assets and imported into APp.js as well as created components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Components/Navbar/Navbar';
 import Banner from './Components/Banner/Banner';
+import Clients from './Components/Clients/Clients';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Banner />
+      <Clients />
     </div>
   );
 }
