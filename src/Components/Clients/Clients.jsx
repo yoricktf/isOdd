ae0111d1770fa3a0e1ef34ce56c37d54ecbb5624
Yorick ten Feld <yorick.tenfeld@gmail.com>
2022-09-15 10:51:00 +0200

    client carousel complete and working with three logos

--- src/Components/Clients/Clients.jsx
+++ src/Components/Clients/Clients.jsx
@@ -1,43 +1,33 @@
 import React from 'react'
+import './Clients.css'
+import LonoLogo from '../../LonoLogo.png'
+import AnustartLogo from '../../AnustartLogo.png'
+import BluthLogo from '../../BluthLogo.png'
 import { Carousel } from 'react-bootstrap'
 
 const Clients = () => {
   return (
-    <Carousel>
+    <Carousel controls={false} indicators={false}>
       <Carousel.Item>
         <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=First slide&bg=373940"
+          className="d-inline companyLogo"
+          src={LonoLogo}
           alt="First slide"
         />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
+          className="d-inline companyLogo"
+          src={AnustartLogo}
           alt="Second slide"
         />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
+          className="d-inline companyLogo"
+          src={BluthLogo}
           alt="Third slide"
         />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
   )
