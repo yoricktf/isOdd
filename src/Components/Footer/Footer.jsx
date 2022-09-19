b0a8f5110fac042b4e722636f6fbba9c5b5ed68e
Yorick ten Feld <yorick.tenfeld@gmail.com>
2022-09-19 09:32:54 +0200

    basic footer html and some minimal styling

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,9 +4,37 @@ import waveBottom from '../../waveBottom.svg'
 
 const Footer = () => {
   return (
-    <footer>
+    <>
       <img src={waveBottom} alt="" />
-    </footer>
+      <footer>
+        <section className=''>
+          <div>
+            <h4>About isOdd</h4>
+            <p>We're passionate about telling you if a number is odd or even so you can focus on more important things</p>
+          </div>
+          <div>
+
+            <h4>Important Links</h4>
+            <ul>
+              <li>Info on odd numbers:</li>
+              <a href="https://mathworld.wolfram.com/OddNumber.html">MathWorld</a>
+              <li>Info on number parity:</li>
+              <a href="https://en.wikipedia.org/wiki/Parity_(mathematics)">Wikipedia</a>
+              <li>Check out isEven:</li>
+              <a href="https://isevenapi.xyz/">isEven</a>
+            </ul>
+          </div>
+          <div>
+            <h4>Contact</h4>
+            <p>123 Fake Street, 13349, Berlin, Germany</p>
+            <a href="mailto:yorick.tenfeld@gmail.com">yorick.tenfeld@gmail.com</a>
+          </div>
+        </section>
+        <hr />
+        <h6>Copyright © 2022 Yorick</h6>
+        <h6>not affliated with wikipedia</h6>
+      </footer>
+    </>
   )
 }
 
