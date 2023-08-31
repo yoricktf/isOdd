bd1134a4fb8e135dead614317778cfbbd6ae6cd7
yoricktf <yorick.tenfeld@gmail.com>
2023-08-31 15:58:59 +0200

    restyled for responsiveness on smaller phones

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const PORT = process.env.PORT || 5050;
 const path = require('path');
 const app = express();
@@ -14,40 +14,37 @@ const adverts = [
   "Feeling down? Why not try a Stop-and-Drop Suicide Booth, America's favourite suicide booth since 2008",
   'Hungry and in a rush? Try Bachelor Chow, now with flavor!',
   "Bean-Bay Beans: They're The Beaniest! Bean-Bay frijoles: los frijoles de los reyes.",
-  "Dunder Mifflin, for all your paper needs",
+  'Dunder Mifflin, for all your paper needs',
   'Saul Goodman & Associates - For all your legal needs, Better Call Saul!',
   "Bob's Burgers, try our burger of the day - The Cauliflower's Cumin From Inside the House Burger (comes with cauliflower & cumin)",
   "The Bluth Company - Getting you in a house you don't need.",
-  "Sabre Printers - For all your printing needs"
-]
+  'Sabre Printers - For all your printing needs',
+];
 
 app.use(express.static(path.resolve(__dirname, '../build')));
 
-app.get("/api", (req, res) => {
-  let number = req.query.number
-  let advertIndex = Math.floor(Math.random() * adverts.length)
+app.get('/api', (req, res) => {
+  let number = req.query.number;
+  let advertIndex = Math.floor(Math.random() * adverts.length);
 
   if (Number.isInteger(Number(number))) {
-    res.json(
-      {
-        ad: adverts[advertIndex],
-        number: number,
-        isodd: number % 2 === 1 ? true : false
-      }
-    )
+    res.json({
+      ad: adverts[advertIndex],
+      number: number,
+      isodd: number % 2 === 1 ? true : false,
+    });
   } else {
-    res.json(
-      {
-        error: "I can't even begin to tell you how silly this input is. Please enter an actual whole number."
-      }
-    )
+    res.json({
+      error:
+        "I can't even begin to tell you how silly this input is. Please enter an actual whole number.",
+    });
   }
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
   });
 }
 
